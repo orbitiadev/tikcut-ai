05e4cfb20517a717a9cb2bf5f4c544ec3bb92f37
orbitiadev <orbit.ia.dev@gmail.com>
2026-08-18 11:10:03 -0300

    Wire Storyverse into TikCut AI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import RootApp from './RootApp';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootApp />
   </StrictMode>
 );
