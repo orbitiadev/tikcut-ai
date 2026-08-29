98b9d795a27c1058ce31f870503f3296575af679
orbitiadev <orbit.ia.dev@gmail.com>
2026-08-29 13:50:13 -0300

    Load only the focused TikCut experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import RootApp from './RootApp';
-import './styles.css';
-import './multicut.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
